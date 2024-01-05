9c5aa023a32a4b9fc27ae6b3c0c4f70505e3d3b2
Rameshwar Jaiswal <122396728+Rameshwarjazz@users.noreply.github.com>
2024-01-05 14:59:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
 import NewsBoard from "./Components/NewsBoard";
-import AuthenticationComponent from "./AuthenticationComponent";
+// import AuthenticationComponent from "./AuthenticationComponent";
 
 const App = () => {
   const [category, setCategory] = useState("general");
