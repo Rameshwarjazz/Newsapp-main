96148d095f05a5d2c1c2218324d66996041bd9fc
Rameshwar Jaiswal <122396728+Rameshwarjazz@users.noreply.github.com>
2024-01-05 15:17:03 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,22 @@
-// import { initializeApp } from 'firebase/app';
-// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDP9f4DidTHLm86fLKTVR4ON6CZk1P2j2c",
-//   authDomain: "newsroom-feb2c.firebaseapp.com",
-//   projectId: "newsroom-feb2c",
-//   storageBucket: "newsroom-feb2c.appspot.com",
-//   messagingSenderId: "127035988793",
-//   appId: "1:127035988793:web:d7b33b53b67cbaa06026e8",
-//   measurementId: "G-KY3272BWY1"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyDP9f4DidTHLm86fLKTVR4ON6CZk1P2j2c",
+  authDomain: "newsroom-feb2c.firebaseapp.com",
+  projectId: "newsroom-feb2c",
+  storageBucket: "newsroom-feb2c.appspot.com",
+  messagingSenderId: "127035988793",
+  appId: "1:127035988793:web:d7b33b53b67cbaa06026e8",
+  measurementId: "G-KY3272BWY1"
+};
 
-//   const app = initializeApp(firebaseConfig);
-//   const auth = getAuth(app);
-//   const firestore = getFirestore(app);
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const firestore = getFirestore(app);
   
-//   export { auth, firestore };
+  export { auth, firestore };
 
 
 
