2c88d970849ab68c43b9b725a544095aa2e3f9db
Rameshwar Jaiswal <122396728+Rameshwarjazz@users.noreply.github.com>
2024-01-05 15:15:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
 import NewsBoard from "./Components/NewsBoard";
-// import AuthenticationComponent from "./AuthenticationComponent";
+import AuthenticationComponent from "./AuthenticationComponent";
 
 const App = () => {
   const [category, setCategory] = useState("general");
 
   return (
     <div>
-{/*       <AuthenticationComponent /> */}
+      <AuthenticationComponent />
       <Navbar setCategory={setCategory} />
       <NewsBoard category={category} />
     </div>
