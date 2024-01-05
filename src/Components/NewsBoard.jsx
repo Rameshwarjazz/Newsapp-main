f680146d3fb9adc1519272ba18dd511f80d86c7d
Rameshwar Jaiswal <122396728+Rameshwarjazz@users.noreply.github.com>
2024-01-05 15:07:24 +0530

    Update NewsBoard.jsx

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -7,6 +7,8 @@ const NewsBoard = ({category}) => {
     useEffect(() => {
         console.log("Fetching news data...");
         let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        url = process.env.NODE_ENV === 'development' ? `http://localhost:3000/${url}` : url;
+
         fetch(url)
           .then((response) => response.json())
           .then((data) => {
